2b01e625b335179c98543ebb36404a022413516e
thomson <thomsonyeh@cmu.edu>
2017-07-31 18:56:38 -0700

    server is working

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-var parser = require('./json-search.js')
+const parser = require('./json-search.js')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('<h2>asdf</h2>')
+})
+
+app.post('/api/search_rule', function (req, res) {
+  console.log(res.body);
+  res.json(parser.parse(req.body['queryString']));
 })
 
 app.listen(3000, function () {
